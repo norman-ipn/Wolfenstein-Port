968afcb1554906c7c15967c1d02323f2d25904e6
AaronRevilla <aaron@aaron-HP-Pavilion-dm1-Notebook-PC.(none)>
2012-11-14 10:43:08 -0600

    fixing the little files

---
+++ SODVER.h
@@ -0,0 +1,13 @@
+/*
+ Control the final compiled version.
+
+ We need to 
+*/
+
+#define SPEAR
+#define ARTSEXTERN
+#define DEMOSEXTERN
+#define CARMACIZED
+//#define MYPROFILE
+//#define DEBCHECK
+//#define UPLOAD
---
+++ WOLF1VER.h
@@ -0,0 +1,16 @@
+/*
+  The code can be compiled to generated diferent version of the game.
+
+  These defines control the final compiled version.
+
+  Need to check the diferences and decide which one we are picking up.
+
+*/
+
+//#define SPEAR
+#define ARTSEXTERN
+#define DEMOSEXTERN
+//#define MYPROFILE
+//#define DEBCHECK
+#define CARMACIZED
+#define UPLOAD
